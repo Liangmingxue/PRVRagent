06be4877cc8db55d38a1b701d98efffdfaf383fb
Liangmingxue <109639113+Liangmingxue@users.noreply.github.com>
2026-09-15 16:10:19 +0800

    chore: export hybrid sidekick temporal utilities

--- src/prvr_agent/video/__init__.py
+++ src/prvr_agent/video/__init__.py
@@ -1,12 +1,24 @@
-from .event_segments import EventSegment, VisualScanPoint, build_event_segments
+from .event_segments import (
+    EventSegment,
+    SemanticScanPoint,
+    SidekickScanPoint,
+    VisualScanPoint,
+    build_event_segments,
+    fuse_sidekick_scans,
+    semantic_scores_to_scan_points,
+)
 from .sampler import DecordFrameSampler, TimeWindow, build_overlapping_windows, uniform_bin_center_indices
 
 __all__ = [
     "DecordFrameSampler",
     "EventSegment",
+    "SemanticScanPoint",
+    "SidekickScanPoint",
     "TimeWindow",
     "VisualScanPoint",
     "build_event_segments",
     "build_overlapping_windows",
+    "fuse_sidekick_scans",
+    "semantic_scores_to_scan_points",
     "uniform_bin_center_indices",
 ]
