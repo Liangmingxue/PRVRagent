dfad2408f21b119bcf9b67e12abaf06b3d2086a2
Liangmingxue <109639113+Liangmingxue@users.noreply.github.com>
2026-09-15 15:47:02 +0800

    chore: export event-aware temporal utilities

--- src/prvr_agent/video/__init__.py
+++ src/prvr_agent/video/__init__.py
@@ -1,3 +1,12 @@
-from .sampler import DecordFrameSampler, TimeWindow, build_overlapping_windows
+from .event_segments import EventSegment, VisualScanPoint, build_event_segments
+from .sampler import DecordFrameSampler, TimeWindow, build_overlapping_windows, uniform_bin_center_indices
 
-__all__ = ["DecordFrameSampler", "TimeWindow", "build_overlapping_windows"]
+__all__ = [
+    "DecordFrameSampler",
+    "EventSegment",
+    "TimeWindow",
+    "VisualScanPoint",
+    "build_event_segments",
+    "build_overlapping_windows",
+    "uniform_bin_center_indices",
+]
