65c2ab79c39fe4ac85f1a2dff2882a32f9e51953
Liangmingxue <109639113+Liangmingxue@users.noreply.github.com>
2026-09-16 10:44:46 +0800

    chore(video): export kernel temporal helpers

--- src/prvr_agent/video/__init__.py
+++ src/prvr_agent/video/__init__.py
@@ -7,18 +7,26 @@ from .event_segments import (
     fuse_sidekick_scans,
     semantic_scores_to_scan_points,
 )
+from .kernel_temporal import (
+    KernelTemporalResult,
+    change_points_to_fractions,
+    kernel_temporal_segment,
+)
 from .sampler import DecordFrameSampler, TimeWindow, build_overlapping_windows, uniform_bin_center_indices
 
 __all__ = [
     "DecordFrameSampler",
     "EventSegment",
+    "KernelTemporalResult",
     "SemanticScanPoint",
     "SidekickScanPoint",
     "TimeWindow",
     "VisualScanPoint",
     "build_event_segments",
     "build_overlapping_windows",
+    "change_points_to_fractions",
     "fuse_sidekick_scans",
+    "kernel_temporal_segment",
     "semantic_scores_to_scan_points",
     "uniform_bin_center_indices",
 ]
